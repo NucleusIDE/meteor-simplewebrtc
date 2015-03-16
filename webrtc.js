d7bf89b1e540d07320fadd8887fa1310044cb066
Charanjit Singh <ckhabra007@gmail.com>
2015-03-16 16:58:59 +0530

    Add a mechanism for sending messages via meteor
    Adds a collection and methods for sending messages to all clients.
    These methods are meant to be used when webrtc is not the main part
    of the app and is not initialized until needed. In such case, use
    these methods to tell the client to turn on the webrtc interface so
    it can connect.
    
     # Please enter the commit message for your changes. Lines starting

---
+++ webrtc.js
@@ -0,0 +1,58 @@
+var WebRtcColl = new Meteor.Collection('webrtc_coll');
+
+var WebrtcExtension = function() {
+  this._messagesAlreadyHandled = [];
+  this._callbacks = [];
+};
+
+WebrtcExtension.prototype.broadcast = function(message, payload) {
+  console.log('Broadcasting', message, payload);
+  WebRtcColl.insert({message: message, payload: payload, created_at: moment().toDate()});
+};
+WebrtcExtension.prototype.on = function(messageType, callback) {
+  this._callbacks[messageType] = this._callbacks[messageType] || [];
+  this._callbacks[messageType].push(callback);
+};
+
+MeteorWebrtc = new WebrtcExtension();
+
+if (Meteor.isClient) {
+  Meteor.subscribe('webrtc_coll_latest_messages');
+
+  //autorun for executing callbacks set on MeteorWebrtc when new messages arrive
+  Tracker.autorun(function() {
+    var newMessages = _.reject(WebRtcColl.find({created_at: {$lt: moment().add({minutes: 10}).toDate()}}, {sort: {created_at: -1}}).fetch(), function(msg) {
+      return _.contains(MeteorWebrtc._messagesAlreadyHandled, msg._id);
+    });
+
+    newMessages.forEach(function(msg) {
+      var callbacks = MeteorWebrtc._callbacks[msg.message];
+
+      if (! callbacks || ! callbacks.length) {
+        return;
+      }
+
+      callbacks.forEach(function(cb) {
+        cb(msg.payload);
+      });
+
+      MeteorWebrtc._messagesAlreadyHandled.push(msg._id);
+    });
+  });
+}
+
+
+if (Meteor.isServer) {
+  WebRtcColl.allow({
+    insert: function() {
+      return true;
+    }
+  });
+
+  Meteor.publish('webrtc_coll_latest_messages', function(limit) {
+    limit = limit || 30;
+    //publish latest 30 messages which are at most 10 minutes old
+    var webrtcMessages = WebRtcColl.find({created_at: {$lt: moment().add({minutes: 10}).toDate()}}, {sort: {created_at: -1}, limit: limit});
+    return webrtcMessages;
+  });
+}
